7fcc21475cdcd3ad6be5d9f0cd529239d3289809
stivenBermeo <bermeopalomares3@gmail.com>
2022-09-03 09:00:57 -0500

    Extending shema

--- src/schema.ts
+++ src/schema.ts
@@ -5,4 +5,40 @@ export const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    postCreate(title: String!, content: String!): PostPayload!
+  }
+
+  type User {
+    id: ID!
+    name: String
+    email: String!
+    profile: Profile
+    posts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    author: User
+  }
+
+  type Profile {
+    id: ID!
+    user: User!
+    bio: String!
+  }
+
+  type UserError {
+    message: String!
+  }
+
+  type PostPayload {
+    userErrors: [UserError!]!,
+    post: Post
+  }
 `;
