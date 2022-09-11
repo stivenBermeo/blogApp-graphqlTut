ec1250465da4136adaeb1d84ee5a336b3d97442b
stivenBermeo <bermeopalomares3@gmail.com>
2022-09-11 08:50:16 -0500

    chore: seeds imp

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -3,14 +3,20 @@ import { PrismaClient } from '@prisma/client'
 const mockUsers = require('./mockData/users.json');
 const mockPosts = require('./mockData/posts.json');
 const mockProfiles = require('./mockData/profiles.json');
+const mockMonolit = require('./mockData/monolit.json');
 
 const prisma = new PrismaClient()
 
 async function main() {
   
-  await prisma.user.createMany({ data: mockUsers })
-  await prisma.post.createMany({ data: mockPosts })
-  await prisma.profile.createMany({ data: mockProfiles })
+  // await prisma.user.createMany({ data: mockUsers })
+  // await prisma.post.createMany({ data: mockPosts })
+  // await prisma.profile.createMany({ data: mockProfiles })
+
+  mockMonolit.map(async (user: any) => {
+    await prisma.user.create({ data: user })
+  })
+
 
 }
 
